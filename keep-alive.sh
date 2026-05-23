9d7288eb52c8a874c07ede71e747ba901051817e
Z User <z@container>
2026-05-23 21:00:36 +0000

    feat: add mini-service for dev server persistence

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-# QMS SaaS Pro - Persistent Server Runner
-# Automatically restarts the Next.js production server if it crashes
-
 cd /home/z/my-project
-
 while true; do
-  echo "[$(date)] Starting QMS SaaS Pro server..."
-  node .next/standalone/server.js
-  EXIT_CODE=$?
-  echo "[$(date)] Server exited with code $EXIT_CODE. Restarting in 2s..."
+  npx next dev -p 3000 -H 0.0.0.0 2>> /home/z/my-project/dev.log
   sleep 2
 done
